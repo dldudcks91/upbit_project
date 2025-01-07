4bce81e23dbf47f96cc7fafc815e5b63c51fe249
Ubuntu <ubuntu@ip-172-31-9-100.ap-northeast-2.compute.internal>
2025-01-07 16:17:49 +0000

    20250108

---
+++ start_upbit_websocket.sh
@@ -0,0 +1,10 @@
+export PYTHONPATH= /home/ubuntu/upbit_project
+export LANG=en_US.UTF-8
+# 메모리 모니터링 스크립트 실행
+#/usr/bin/python3 /home/ubuntu/baseball_project/system_monitor/memory__monitor.py &
+# CPU 모니터링 스크립트 실행
+#/usr/bin/python3 /home/ubuntu/baseball_project/system_monitor/cpu_monitor.py &
+# 백그라운드 프로세스들이 종료되지 않도록 대기
+/usr/bin/python3 /home/ubuntu/upbit_project/compile.py &
+wait
+
