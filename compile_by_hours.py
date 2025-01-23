b634ae58adb26c7f2891f2accc88a9bafc11b3bf
dldudcks91 <dldudcks91@gmail.com>
2025-01-24 02:26:51 +0900

    'hi'

--- compile_by_hours.py
+++ compile_by_hours.py
@@ -3,7 +3,8 @@ import requests
 from datetime import datetime , timedelta
 import time
 import pandas as pd
-
+import yaml
+import pymysql
 
 def get_krw_markets():
     """업비트 KRW 마켓의 모든 거래쌍 조회"""
@@ -13,7 +14,6 @@ def get_krw_markets():
     # KRW 마켓만 필터링
     krw_markets = [market['market'] for market in markets if market['market'].startswith('KRW-')]
     print(f"Total KRW markets: {len(krw_markets)}")
-    krw_markets.remove('KRW-BTC')
     return krw_markets
 
 
@@ -24,14 +24,16 @@ markets = get_krw_markets()
 #%%
 old_list = list()
 for market in markets:
-    start_date = datetime(2025, 1, 21)    
+    current_time = datetime.now()
+    start_date = current_time.replace(minute=0, second=0, microsecond=0) - timedelta(hours=1)
+    date_str = start_date.strftime('%Y-%m-%d %H:%M:%S')
     for hour_range in range(1):
-        date_str = start_date.strftime('%Y-%m-%d %H:%M:%S')
+        
         # Change endpoint to hourly candles
         url = 'https://api.upbit.com/v1/candles/minutes/60'
         params = {
             'market': market,
-            'count': 24,  # Each request will get 100 hours of data
+            'count': 2,  # Each request will get 100 hours of data
             'to': date_str
         }
         
@@ -41,23 +43,13 @@ for market in markets:
         new_list = response.json()
         old_list.extend(new_list)
         # Adjust time delta to move back 100 hours instead of 100 days
-        start_date -= timedelta(hours=100)
+        
         
         print(market, len(old_list), start_date.strftime('%Y-%m-%d %H:%M:%S'))
         
-
 #%%
-total_list = list()
-for data in old_list:
-    if data == 'name':
-        continue
-    date = data['candle_date_time_utc']
-    market = data['market'][4:]  # Remove 'KRW-' prefix
-    price = data['opening_price']
-    volume = data['candle_acc_trade_volume']
-    amount = data['candle_acc_trade_price']
-    
-    total_list.append([date, market, price, volume, amount])
+
+tables = ['tb_market_hour']
 #%%
 total_list = list()
 for data in old_list:
@@ -67,33 +59,39 @@ for data in old_list:
     date = data['candle_date_time_utc'].replace('T', ' ')
     market = data['market']
     opening_price = data['opening_price']
+    trade_price = data['trade_price']
     high_price = data['high_price']
+    low_price = data['low_price']
     volume = data['candle_acc_trade_volume']
     amount = data['candle_acc_trade_price']
     
-    total_list.append([date, market,opening_price, high_price, volume,amount])
+    total_list.append([date, market,opening_price, trade_price, high_price, low_price, volume,amount])
     
-    #%%
+#%%
 df = pd.DataFrame(total_list)
-df.columns = ['log_dt','market','opening_price','high_price','volume','amount']
+df.columns = ['log_dt','market','opening_price','trade_price','high_price','low_pridce','volume','amount']
 df = df.sort_values(by = 'market')
 df_unique = df.drop_duplicates(subset=['log_dt', 'market'])
 #%%
-price_pivot = df_unique.pivot(index='log_dt', columns='market', values='opening_price')
-corr_matrix = price_pivot.corr()
+# price_pivot = df_unique.pivot(index='log_dt', columns='market', values='opening_price')
+# corr_matrix = price_pivot.corr()
+# duplicates = df[df.duplicated(keep=False)] 
 #%%
-duplicates = df[df.duplicated(keep=False)] 
-#%%
-import pymysql
+file_path = "/home/ubuntu/baseball_project/db_settings.yml"  # YAML 파일이 있는 폴더 경로
+with open(file_path, 'r', encoding = 'utf-8') as file:
+    yaml_data = yaml.safe_load(file)
+    yaml_data = yaml_data['BASEBALL']
+    
 conn = pymysql.connect(
-host='127.0.0.1',  # 로컬호스트
-port=3306,  # 동적으로 할당된 로컬 포트 
-user='root',
-passwd='an98',
-db='upbit')
-tables = ['tb_market_hour']
+   host=yaml_data['HOST'],
+   user=yaml_data['USER'],
+   password=yaml_data['PASSWORD'],
+   db= 'upbit'
+)
+
+
 with conn.cursor() as cursor:
-    cursor.execute("SET FOREIGN_KEY_CHECKS = 0")
+    
     
     for table in tables:
         cursor.execute(f"SHOW COLUMNS FROM {table}")
@@ -115,8 +113,4 @@ with conn.cursor() as cursor:
                 continue
     
         print(f'success migration table {table}')
-    conn.commit()
-#%%
-with conn.cursor() as cursor:
-    cursor.execute("SET FOREIGN_KEY_CHECKS = 1")
     conn.commit()
