f1196887cb73d55c3d9d7e2986c5aac9d8f2a44c
dldudcks91 <dldudcks91@gmail.com>
2025-01-28 20:02:42 +0900

    hi

--- start_upbit_websocket.sh
+++ start_upbit_websocket.sh
@@ -2,11 +2,12 @@
 
 export PYTHONPATH=/home/ubuntu/upbit_project
 export LANG=en_US.UTF-8
-# 메모리 모니터링 스크립트 실행
-#/usr/bin/python3 /home/ubuntu/baseball_project/system_monitor/memory__monitor.py &
-# CPU 모니터링 스크립트 실행
-#/usr/bin/python3 /home/ubuntu/baseball_project/system_monitor/cpu_monitor.py &
-# 백그라운드 프로세스들이 종료되지 않도록 대기
+
+# 웹소켓 스크립트
 /usr/bin/python3 /home/ubuntu/upbit_project/upbit_websocket.py &
+
+# 가격 데이터 수집 스크립트 실행
+/usr/bin/python3 /home/ubuntu/upbit_project/upbit_by_seconds.py &
+
 wait
 
