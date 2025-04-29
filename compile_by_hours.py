a0b364cef3427806c78264de260b3f14da4d070e
dldudcks91 <dldudcks91@gmail.com>
2025-04-30 02:41:16 +0900

    add ma_logic

--- compile_by_hours.py
+++ compile_by_hours.py
@@ -30,10 +30,10 @@ for market in markets:
     for hour_range in range(1):
         
         # Change endpoint to hourly candles
-        url = 'https://api.upbit.com/v1/candles/minutes/60'
+        url = 'https://api.upbit.com/v1/candles/minutes/15'
         params = {
             'market': market,
-            'count': 1,  # Each request will get 100 hours of data
+            'count': 15,  # Each request will get 100 hours of data
             'to': date_str
         }
         
@@ -114,3 +114,102 @@ with conn.cursor() as cursor:
     
         print(f'success migration table {table}')
     conn.commit()
+#%%
+
+# ma_데이터 가져오기
+data_dic = dict()
+
+
+
+now = (datetime.now() - timedelta(hours=1) - timedelta(hours = 9))
+
+ma_dic = dict()
+with conn.cursor() as cursor:
+    
+    
+    time_list = [10, 20, 34, 50 ,100, 200, 400, 800]
+       
+    for time in time_list:        
+        ago = (now - timedelta(hours=time)).strftime('%Y-%m-%d %H:00:00')      
+        cursor.execute(f"SELECT market, avg(trade_price) as ma FROM tb_market_hour WHERE log_dt > '{ago}' group by market")
+            
+        ma_data = pd.DataFrame(cursor.fetchall())
+        
+        ma_dic[time] = ma_data
+    one_hour_ago = (now - timedelta(hours = 1)).strftime('%Y-%m-%d %H:00:00')      
+    cursor.execute(f"SELECT * FROM tb_ma_60_minutes where log_dt = '{one_hour_ago}'")
+    last_ma_data = pd.DataFrame(cursor.fetchall())
+            
+    
+            
+
+conn.close()
+#%%
+    
+markets = list(ma_data.iloc[:,0])
+market_ma_dic = {market:[now.strftime('%Y-%m-%d %H:00:00')] for market in markets}
+
+for market in markets:
+    for time_ma in time_list:
+        ma_time_data = ma_dic[time_ma]
+        price_ma = ma_time_data[ma_time_data.iloc[:,0] == market].iloc[0,1]
+        market_ma_dic[market].append(price_ma)
+        
+        if time_ma == 10:
+            new_ma_10 = price_ma
+        elif time_ma == 34:
+            new_ma_34 = price_ma
+    
+    try:
+        last_ma_market_data = last_ma_data[last_ma_data.iloc[:,0] == market]
+        old_ma_10 = last_ma_market_data.iloc[0,2]
+        old_ma_34 = last_ma_market_data.iloc[0,4]
+    except:
+        old_ma_10 = 0
+        old_ma_34 = 0
+    
+    
+    if (old_ma_10 < old_ma_34) & (new_ma_10 >= new_ma_34):
+        is_golden_cross = 1
+    else:
+        is_golden_cross = 0
+        
+    if (old_ma_10 > old_ma_34) & (new_ma_10 <= new_ma_34):
+        is_dead_cross = 1
+    else:
+        is_dead_cross = 0
+    
+    market_ma_dic[market].append(is_golden_cross)
+    market_ma_dic[market].append(is_dead_cross)
+    market_ma_dic[market].append(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+#%%
+input_data = pd.DataFrame(market_ma_dic).transpose().reset_index()
+column_names = ['market','log_dt','ma_10','ma_20','ma_34','ma_50','ma_100','ma_200','ma_400','ma_800','golden_cross_10_34','dead_cross_10_34','created_at']
+input_data.columns = column_names
+#%%
+
+    
+with conn.cursor() as cursor:
+    
+    
+    
+        
+    
+    for _, row in input_data.iterrows():
+        # 동적으로 컬럼과 값 생성
+        columns = ', '.join(column_names)
+        placeholders = ', '.join(['%s'] * len(column_names))
+        
+        
+        sql = f"INSERT INTO tb_ma_60_minutes ({columns}) VALUES ({placeholders})"
+        
+        # 각 행 삽입
+        
+        cursor.execute(sql, tuple(row))
+        
+        
+
+    
+conn.commit()
+
+
