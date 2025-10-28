ef45d99e7ea364b64fd71927900b7700020a395b
LYC <dldudcks91@gmail.com>
2025-10-28 16:22:49 +0900

    hi

--- bitget/kakao_message_sender.py
+++ bitget/kakao_message_sender.py
@@ -19,7 +19,7 @@ from datetime import datetime, timedelta
 class KakaoMessageSender:
     """카카오톡 메시지 전송 클래스"""
     
-    def __init__(self, rest_api_key, redirect_uri, client_secret=None, token_file="/home/ubuntu/upbit_project/bitget/kakao_token.json "): #kakao_token.json
+    def __init__(self, rest_api_key, redirect_uri, client_secret=None, token_file="/home/ubuntu/upbit_project/bitget/kakao_token.json"): #kakao_token.json
         """
         초기화
         
